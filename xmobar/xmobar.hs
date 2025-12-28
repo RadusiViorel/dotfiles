01c3744fb65e971a32375d64439c6add8f70ebcb
Radusi Viorel <radusi.viorel@gmail.com>
2025-12-28 07:13:23 +0200

    changed xmobar conf

--- xmobar/xmobar.hs
+++ xmobar/xmobar.hs
@@ -4,12 +4,16 @@ import Data.Monoid ((<>))
 dir :: String
 dir="/home/radusiviorel/.config/xmobar"
 
-click :: String -> Int -> String -> String
-click cmd btn content = "<action=`" <> cmd <> "` button=" <> show btn <> ">" <> content <> "</action>"
+cmd :: Bool -> String -> [String] -> Int -> Runnable
+cmd True comp args rate =
+  Run $ Com (dir <> "/com/" <> comp <> "/index") args comp rate
+cmd False comp  _  _  =
+  Run $ Com "echo" [""] comp 100000
 
-enabled :: Bool -> Command -> Runnable
-enabled True  comp = Run $ comp
-enabled False _    = Run $ Com "echo" [] "" 1000000
+click :: String -> String -> Int -> String -> String
+click cmd action btn content =
+  "<action=`" <> dir <> "/com/" <> cmd <> "/index " <> action <> "` button=" <> show btn <> ">" <> content <> "</action>"
+
 
 main :: IO ()
 main = xmobar defaultConfig
@@ -23,47 +27,47 @@ main = xmobar defaultConfig
   , allDesktops = True
   , overrideRedirect = True
   , commands =
-      [ enabled True  $ Com ( dir ++ "/com/capslock/index"  ) [] "capslock" 10
-      , enabled True  $ Com ( dir ++ "/com/updates/index"   ) [] "updates" 3600
-      , enabled True  $ Com ( dir ++ "/com/page/index"      ) [] "page" 10
-      , enabled True  $ Com ( dir ++ "/com/volume/index"    ) [] "volume" 5
-      , enabled True  $ Com ( dir ++ "/com/brightness/index") [] "brightness" 5
-      , enabled True  $ Com ( dir ++ "/com/bluetooth/index" ) [] "bluetooth" 5
-      , enabled True  $ Com ( dir ++ "/com/battery/index"   ) [] "battery" 5
-      , enabled True  $ Com ( dir ++ "/com/cpu_temp/index"  ) [] "cpu_temp" 50
-      , enabled True  $ Com ( dir ++ "/com/space/index"     ) [] "space" 20
-      , enabled True  $ Com ( dir ++ "/com/network/index"   ) [] "network" 5
-      , enabled True  $ Com ( dir ++ "/com/rofi_theme/index") [] "rofi_theme" 1000
-      , enabled True  $ Com ( dir ++ "/com/calendar/index"  ) [] "calendar" 10
-      , enabled True  $ Com ( dir ++ "/com/power/index"     ) [] "power" 1000
+      [ cmd True  "capslock"   [] 10
+      , cmd True  "updates"    [] 3600
+      , cmd True  "page"       [] 10
+      , cmd True  "volume"     [] 5
+      , cmd True  "brightness" [] 5
+      , cmd True  "bluetooth"  [] 5
+      , cmd True  "battery"    [] 5
+      , cmd True  "cpu_temp"   [] 50
+      , cmd True  "space"      [] 20
+      , cmd True  "network"    [] 5
+      , cmd True  "rofi_theme" [] 1000
+      , cmd True  "calendar"   [] 10
+      , cmd True  "power"      [] 1000
       , Run UnsafeXMonadLog
       ]
   , sepChar = "%"
   , alignSep = "}{"
   , template = mconcat
       [ "%UnsafeXMonadLog% }{ "
-      ,                                                        "%capslock%"
-      , click ( dir ++ "/com/updates/index show"           ) 1 "%updates%"
-      , click ( dir ++ "/com/volume/index volume_jump"     ) 1
-      $ click ( dir ++ "/com/volume/index mute"            ) 2
-      $ click ( dir ++ "/com/volume/index toggle"          ) 3
-      $ click ( dir ++ "/com/volume/index volume_up"       ) 4
-      $ click ( dir ++ "/com/volume/index volume_down"     ) 5 "%volume%"
-      , click ( dir ++ "/com/brightness/index up 3"        ) 4
-      $ click ( dir ++ "/com/brightness/index down 3"      ) 5 "%brightness%"
-      , click ( dir ++ "/com/bluetooth/index open"         ) 1
-      $ click ( dir ++ "/com/bluetooth/index toggle"       ) 2
-      $ click ( dir ++ "/com/bluetooth/index reset"        ) 3
-      $ click ( dir ++ "/com/bluetooth/index cycle_up"     ) 4
-      $ click ( dir ++ "/com/bluetooth/index cycle_down"   ) 5 "%bluetooth%"
-      , click ( dir ++ "/com/battery/index show_time"      ) 1 "%battery%"
-      , click ( dir ++ "/com/cpu_temp/index top"           ) 1 "%cpu_temp%"
-      , click ( dir ++ "/com/space/index toggle"           ) 1 "%space%"
-      , click ( dir ++ "/com/network/index connect"        ) 1 "%network%"
-      , click ( dir ++ "/com/rofi_theme/index change"      ) 1 "%rofi_theme%"
-      , click ( dir ++ "/com/page/index toggle"            ) 1 "%page%"
-      , click ( dir ++ "/com/calendar/index toggle"        ) 1
-      $ click ( dir ++ "/com/calendar/index show_calendar" ) 3 "%calendar%"
-      , click ( dir ++ "/com/power/index open"             ) 1 "%power%"
+      ,                                       "%capslock%"
+      ,  click "updates"    "show"          1 "%updates%"
+      ,  click "volume"     "volume_jump"   1
+      $  click "volume"     "mute"          2
+      $  click "volume"     "toggle"        3
+      $  click "volume"     "volume_up"     4
+      $  click "volume"     "volume_down"   5 "%volume%"
+      ,  click "brightness" "up 3"          4
+      $  click "brightness" "down 3"        5 "%brightness%"
+      ,  click "bluetooth"  "open"          1
+      $  click "bluetooth"  "toggle"        2
+      $  click "bluetooth"  "reset"         3
+      $  click "bluetooth"  "cycle_up"      4
+      $  click "bluetooth"  "cycle_down"    5 "%bluetooth%"
+      ,  click "battery"    "show_time"     1 "%battery%"
+      ,  click "cpu_temp"   "top"           1 "%cpu_temp%"
+      ,  click "space"      "toggle"        1 "%space%"
+      ,  click "network"    "connect"       1 "%network%"
+      ,  click "rofi_theme" "change"        1 "%rofi_theme%"
+      ,  click "page"       "toggle"        1 "%page%"
+      ,  click "calendar"   "toggle"        1
+      $  click "calendar"   "show_calendar" 3 "%calendar%"
+      ,  click "power"      "open"          1 "%power%"
       ]
   }
